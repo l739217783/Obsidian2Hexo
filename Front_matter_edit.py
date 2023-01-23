f15c14508e4f6228646dce21a9d95e7cb3e90d29
lin <739217783@qq.com>
2023-01-23 15:28:30 +0800

    yaml_list2dict方法优化，改用正则识别空格+换行，忽略YAML折叠写法，增加程序健壮性

--- Front_matter_edit.py
+++ Front_matter_edit.py
@@ -14,7 +14,7 @@ TODO:字段（标签）是否有#，有的去除
 TODO:使用正则检测时间格式是否符合特定格式
 
 """
-
+import os
 import re
 from functools import wraps
 
@@ -197,27 +197,47 @@ def replce_shuangyin(yaml_content, body):
 
 
 def yaml_list2dict() -> dict:
-    """将yaml列表转换为字典"""
+    r"""将yaml列表转换为字典
+    一些解释
+    匹配属性解释
+        属性名是英文(大小写)+空格
+        必须字母开头，个人书写可能有空格开头,如果不以英文字母开头，可能会匹配到折叠写法的超链接`  http:xxxxxx`
+    方法使用解释
+        r_key正则匹配模式,必须采用match,如果采用search,From属性下的超链接协议http:会给匹配到,会造成值被当成属性匹配了
+    值如果采用折叠写法`  - >-\n`会忽略掉
+    批量使用的话,会忽略excalidraw插件产生的文件,检测标准:无法提取Medata(抛出KeyError)+文件名包含excalidraw
+    """
 
-    r_key = re.compile(r'[a-z\s]+[:]')
-    yaml_content = get_info()['yaml']
+    r_key = re.compile(r'^[a-zA-Z][a-zA-Z\s]+[:]')  # 匹配属性
+    space_line = re.compile(r'\s*[\n]')  # 匹配空格+换行
+
+    try:
+        yaml_content = get_info()['yaml']
+    except TypeError:
+        return {}  # 提取错误，证明无Medate,直接返回空字典
+
     yaml_dict = {}
     yaml_key = None
     yaml_value = None
 
     for index, value in enumerate(yaml_content):
         try:
-            if r_key.search(value) and value.find('---') == -1:
+            if r_key.match(value) and value.find('---') == -1:
                 yaml_key, yaml_value = value.split(':', 1)
-                # 值如果换行,就是数组,反之就是单个(直接写入字典)
-                if yaml_value == ' \n' or yaml_value == '\n':
+                if space_line.match(yaml_value):  # 匹配Medata属性类型就是数组,反之就是单个
                     yaml_dict[yaml_key] = []
                 else:
                     yaml_dict[yaml_key] = yaml_value.strip()
-            elif value.find('-') > -1 and value.find('---') == -1:
+            elif value.find('-') > -1 and value.find('---') == -1:  # 数组写法,且不是---
+                if value.find('- >-') > -1:
+                    continue  # 检测YAML是否采用折叠写法，是忽略
                 yaml_dict[yaml_key].append(value.replace('-', '').strip())
         except AttributeError:
-            raise Exception('书写不规范,空值情况下,且冒号后面有多个空格')
+            raise Exception('书写不规范')
+        except KeyError:
+            if os.path.split(file_path)[1].find('excalidraw') > -1:
+                # raise Exception('不支持excalidraw文件')
+                continue
     return yaml_dict
 
 
@@ -277,5 +297,5 @@ if __name__ == '__main__':
     #             edit_attr()
 
     # print(get_tags())
-    file_path = r"C:\0系统库\桌面\新建文件夹 (3)\回路：设计人生的增长引擎.md"
+    file_path = r"C:\0系统库\桌面\新建文件夹 (3)\剪切板的调用.md"
     print(yaml_list2dict())
